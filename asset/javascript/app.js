b3e4b0edc112d1a33fa5e1cac416a5d89f4ac8db
AnicetChiza <chizaanicet@gmail.com>
2024-07-30 17:04:43 +0200

    add to my repository

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -12,3 +12,33 @@ jQuery(function ($) {
         }
     });
 });
+
+/*------------------------------
+#For our counter
+/-----------------------------*/
+
+document.addEventListener('DOMContentLoaded', startValueAnimation);
+
+function startValueAnimation() {
+    let valueDisplays = document.querySelectorAll('.num');
+    let interval = 4000;
+
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute('data-Val'));
+        let decimalPart = endValue % 1;
+        let steps = Math.floor(endValue);
+        let stepDuration = interval / steps;
+        let counter = setInterval(function () {
+            if (startValue < steps) {
+                startValue += 1;
+                valueDisplay.textContent = startValue;
+            } else if (startValue === steps && decimalPart > 0) {
+                startValue += decimalPart;
+                valueDisplay.textContent = endValue.toFixed(1);
+            } else {
+                clearInterval(counter);
+            }
+        }, stepDuration);
+    });
+}
