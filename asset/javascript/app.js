ba58e218a2c311cef8e8da9a2fc8af329f4ee99d
AnicetChiza <chizaanicet@gmail.com>
2024-08-14 18:07:38 +0200

    add to my repository

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -140,24 +140,3 @@ window.onclick = function (event) {
         });
     }
 };
-
-/*--------------------------/
-#Increase number
----------------------------*/
-
-const minusBtn = document.querySelector('.bi-dash-circle');
-const plusBtn = document.querySelector('.bi-plus-circle');
-const numberDisplay = document.querySelector('.number span');
-
-minusBtn.addEventListener('click', () => {
-    let currentNumber = parseInt(numberDisplay.textContent);
-    if (currentNumber > 0) {
-        numberDisplay.textContent = currentNumber - 1;
-    }
-});
-
-
-plusBtn.addEventListener('click', () => {
-    let currentNumber = parseInt(numberDisplay.textContent);
-    numberDisplay.textContent = currentNumber + 1;
-});
