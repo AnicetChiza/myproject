00202ef39769bcc6b276480eca430f6ee1f493a3
Anicet Chiza <chizaanicet@gmail.com>
2024-11-21 11:04:39 +0200

    add to my repository

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,19 +1,4 @@
 /*------------------------------
-#For the header
-/-----------------------------*/
-jQuery(function ($) {
-    const $header = $('.header');
-    $(window).scroll(function (event) {
-        const $current = $(this).scrollTop();
-        if ($current > 160) {
-            $header.addClass('header-color');
-        } else {
-            $header.removeClass('header-color');
-        }
-    });
-});
-
-/*------------------------------
 #For our counter
 /-----------------------------*/
 
--- asset/javascript/preload.js
+++
@@ -1,7 +0,0 @@
-/*-------------------------/
-#Preloader
-/-------------------------*/
-setTimeout(function () {
-    const preloader = document.querySelector('.preloader');
-    preloader.style.display = 'none';
-}, 3000);
